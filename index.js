e6271fd9bb187bc29c250dccb50d9794f4f92698
Rhonda Impey <rhonda.impey@ping-works.com.au>
2018-11-20 00:27:21 +1100

    Albums Listing

--- index.js
+++ index.js
@@ -12,14 +12,38 @@ jokes.getOne()
 $("#reset").click(function(){
     document.location.reload(true) });
 
+
+
+// click hamburger to show mobile menu
 const hamburger = document.querySelector('.hamburger');
 const body = document.querySelector("body")
 
 hamburger.onclick = function(event) {
     event.preventDefault();
 
-    if (body.className === "nav-open")
-        body.className = "";
-    else
-        body.className = "nav-open";
+    body.classList.toggle("nav-open");
 };
+
+
+// click Go button to display list of songs
+const goButton = document.getElementById('go');
+
+goButton.onclick = function(event) {
+    event.preventDefault();
+
+    if (body.classList.contains("list-albums")) {
+        body.classList.remove("list-albums", "list-albums-loaded");
+    } else {
+      body.classList.add("list-albums");
+
+      setTimeout(() => {
+        body.classList.add("list-albums-loading");
+
+        setTimeout(() => {
+          body.classList.remove("list-albums-loading");
+          body.classList.add("list-albums-loaded");
+        }, 2000);
+
+      }, 300);
+    }
+}
